ea855359a47c50a9c860222d835e6f69941cf378
rpc08002 <rcurtin0491@gmail.com>
2020-12-09 17:32:31 -0500

    fixed indentation

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+let APIKey = "cadd0cedb3b71aa5a82adadf09d49ac6";
 
-function searchCities(city) {
-    // Weather API Key
-    var APIKey = "cadd0cedb3b71aa5a82adadf09d49ac6"
 
-    // Building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + "q=Atlanta&appid=" + APIKey;
 
-    // Run AJAX Call To The OpenWeatherMap API
+function requestWeather() {
+
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
+
     $.ajax({
-    url: queryURL,
-    method: "GET"
-    }).then(function(response) {
-        
-        console.log(queryURL);
-});
-}
+        url:queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+        var weatherApiResponse = response;
+
+        lon = response.coord.lon;
+        lat = response.coord.lat;
+
+        queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            var uvApiResponse = response;
+            console.log("UV", uvApiResponse );
+            renderCurrentWeather(weatherApiResponse, uvApiResponse);
+        })
+    });
+
+};
