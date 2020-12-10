ec0931e60d0dd14829cb5cb4213bb6c33c0d6908
rpc08002 <rcurtin0491@gmail.com>
2020-12-10 13:16:49 -0500

    added UV index ajax call

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ searchButton.click(function () {
 
             var local = localStorage.setItem(keyCount, response.name);
             keyCount = keyCount + 1;
-
+            // Appending Searching History
             var currentCard = $(".currentCard").append("<div>").addClass("card-body");
             currentCard.empty();
             var currentName = currentCard.append("<p>");
@@ -47,14 +47,32 @@ searchButton.click(function () {
             var timeUTC = new Date(response.dt * 1000);
             currentName.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
             currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+
             // Add Temp 
             var currentTemp = currentName.append("<p>");
-            // .addClass("card-text");
+
+            // Appending Current Temperature Responses
             currentName.append(currentTemp);
             currentTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
             currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
             currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
 
             // UV Index URL
-            var urlUV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+            var urlUV = `https://api.openweathermap.org/data/2.5/uvi?appid=cadd0cedb3b71aa5a82adadf09d49ac6&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+
+            // UV Index
+            $.ajax({
+                url: urlUV,
+                method: "GET"
+            }).then(function (response) {
+
+                var currentUV = currentTemp.append("<p>" + "UV Index: " + response.value + "</p>").addClass("card-text");
+                currentUV.addClass("UV");
+                currentTemp.append(currentUV);
+                // currentUV.append("UV Index: " + response.value);
+            });
+
+
+
+
 
